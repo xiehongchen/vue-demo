c20c3604e881d19dc28797dafc4cc2e87728cc08
xiehongchen <1754581057@qq.com>
2024-03-04 16:11:44 +0800

    初始化配置

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+/*
+ * @Author: xiehongchen 1754581057@qq.com
+ * @Date: 2024-03-04 15:06:00
+ * @LastEditors: xiehongchen 1754581057@qq.com
+ * @LastEditTime: 2024-03-04 16:11:33
+ * @FilePath: /vue-demo/src/router/index.ts
+ * @Description: 
+ * 认真学习每一天
+ */
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+ {
+  path: '/table',
+  component: () => import('@/views/table.vue')
+ },
+ {
+  path: '/axios',
+  component: () => import('@/views/axios.vue')
+ }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior: () => ({ left: 0, top: 0 })
+})
+
+export default router
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+
+  return {
+    plugins: [
+      vue()
+    ],
+    // 别名
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  }
 })
