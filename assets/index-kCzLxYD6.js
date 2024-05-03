e408872f71bf35d538f80b836ca7eec898a40bf1
xiehongchen <1754581057@qq.com>
2024-05-03 16:17:02 +0000

    Deploying to gh-pages from @ xiehongchen/vue-demo@026dde1099ac5589efd370c14448da7c95f91d0f 🚀

---
+++ assets/index-kCzLxYD6.js
@@ -0,0 +1,2 @@
+import{g as s,u as a,o as n,h as t,i as e,F as o}from"./vendor-5QtJxRnT.js";const u=e("div",null,"主页",-1),r=s({__name:"index",setup(s){const r=a(),l=()=>{r.push({name:"doc",params:{name:"test"}})};return(s,a)=>(n(),t(o,null,[u,e("button",{onClick:l},"点击")],64))}});export{r as default};
+//# sourceMappingURL=index-kCzLxYD6.js.map
---
+++ assets/index-nRRdfOZL.js
@@ -0,0 +1,2 @@
+import{_ as r}from"./index-8K_HboOq.js";import{o,h as n}from"./vendor-5QtJxRnT.js";const e=r({},[["render",function(r,e){return o(),n("div",null,"封装组件")}]]);export{e as default};
+//# sourceMappingURL=index-nRRdfOZL.js.map
