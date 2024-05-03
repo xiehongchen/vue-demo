026dde1099ac5589efd370c14448da7c95f91d0f
xiehongchen <1754581057@qq.com>
2024-05-04 00:15:56 +0800

    fix

--- src/directives/highlight.ts
+++ src/directives/highlight.ts
@@ -1,12 +1,12 @@
-import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css' //样式
+// import hljs from 'highlight.js';
+// import 'highlight.js/styles/atom-one-dark.css' //样式
 
-//创建v-highlight全局指令
-export default {
-  mounted(el: any) {
-    const blocks = el.querySelectorAll('pre code');
-    blocks.forEach((block: HTMLDivElement) => {
-      hljs.highlightElement(block);
-    });
-  }
-}
+// //创建v-highlight全局指令
+// export default {
+//   mounted(el: any) {
+//     const blocks = el.querySelectorAll('pre code');
+//     blocks.forEach((block: HTMLDivElement) => {
+//       hljs.highlightElement(block);
+//     });
+//   }
+// }
